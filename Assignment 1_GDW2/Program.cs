30e1bba1b519a8562b1c7db2be30b9546a4f00c3
Claratatoes <hasnad10@gmail.com>
2023-10-05 18:27:51 +0500

    first commit

---
+++ Assignment 1_GDW2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+internal class Assignment
+{
+    public static void  Add(int a, int b)
+    {
+        int result = a + b;
+        Console.WriteLine(result);
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.ReadLine(a, b);
+        string = "AdditionResult";
+
+        Console.WriteLine(AdditionResult);
+    }
+}
+
+}
